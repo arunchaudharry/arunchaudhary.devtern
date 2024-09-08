6124ebfcb56ca57778cf304cae3b71f9bc410b2d
arunchaudharry <arunchaudhary6789@gmail.com>
2024-09-08 09:38:59 +0530

    Add files via upload

---
+++ __pycache__/main.py
@@ -0,0 +1,70 @@
+import speech_recognition as sr 
+import webbrowser
+import pyttsx3  
+import musiclibrary
+import requests
+
+#pip install pocketsphinx and google sr
+
+recognizer = sr.Recognizer()
+engine = pyttsx3.init()
+newsapi = ""
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+        
+def process_command(c):
+    if "open brave" in c.lower():
+        webbrowser.open("https://brave.com")
+    elif "open whatsapp" in c.lower():
+        webbrowser.open("https://whatsapp.com")
+    elif "open youtube" in c.lower():
+        webbrowser.open("https://youtube.com") 
+    elif "open linkedin" in c.lower():
+        webbrowser.open("https://linkedin.com") 
+    elif c.lower().startswith("play"):
+        song = c.lower().split(" ")[1]
+        link = musiclibrary.music[song]
+        webbrowser.open(link)
+ 
+    elif "news" in c.lower():
+        r = requests.get("https://newsapi.org/v2/top-headlines?country=ina&apiKey=c8320e29b762415a9a065c28a1603ab5")
+        if r.status_code == 200:
+            # Parse the JSON response
+            data = r.json()
+ 
+            # Extract the articles
+            articles = data.get('articles', [])        
+
+            # Print the headlines
+            for article in articles:
+               speak(article['title'])
+
+if __name__ == "__main__":
+    speak("Initializing Battery....")
+    print("Initializing Battery...")
+    while True:
+        # Listen for the wake word "Battery"
+        # obtain audio from the microphone
+        r = sr.Recognizer()
+        
+        print("recognizing...")
+        try:
+            with sr.Microphone() as source:
+                print("Listening...")
+                audio = r.listen(source, timeout=2, phrase_time_limit=1)
+            word = r.recognize_google(audio)
+            if(word.lower() == "battery"):
+                speak("Ya")
+                # Listen for command
+                with sr.Microphone() as source:
+                    print(" Active Battery...")
+                    audio = r.listen(source)
+                    command = r.recognize_google(audio)
+
+                    process_command(command)
+
+        except Exception as e:
+            print("Error; {0}".format(e))  
+
