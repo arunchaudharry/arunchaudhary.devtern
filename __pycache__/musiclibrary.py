aa7a95f947fb8427c5630da94eb4d1614a03e6b7
arunchaudharry <arunchaudhary6789@gmail.com>
2024-09-08 09:39:29 +0530

    Add files via upload

---
+++ __pycache__/musiclibrary.py
@@ -0,0 +1,6 @@
+music = {
+    "jaat": "https://www.youtube.com/watch?v=QJdw4OUWyRY?si=YzL7je3v2qfDKrI_",
+    "hukumat": "https://www.youtube.com/watch?v=c2qQKBdkYuc?si=ufqkenOsQr3HyCBE",
+    "fein": "https://www.youtube.com/watch?v=B9synWjqBn8?si=B4ezzZuxsY8r3kio",
+    "backbite": "https://www.youtube.com/watch?v=kDlF4cRscHo?si=A3Uo04riQEv-R3V0"
+}
